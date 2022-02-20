cf6a19af28e11b4c13ac8f45c2f904a17631e974
Alazar Zewde <ayzewde@gmail.com>
2022-02-20 17:31:18 -0600

    add axios and implement api service layer

---
+++ src/api/index.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosResponse } from 'axios'
+import { TypeFlags } from 'typescript'
+import { Route, Direction, Stop, Departure } from '../types/index'
+
+const baseUrl: string = 'https://svc.metrotransit.org/nextripv2'
+
+export const getRoutes = async (): Promise<Route[]> => {
+    return await axios.get<Route[]>(`${baseUrl}/routes/`)
+    .then(response => response.data)
+    .catch(error => error)
+}
+
+export const getDirections = async (routeId: string): Promise<Direction[]> => {
+    return await axios.get<Direction[]>(`${baseUrl}/directions/${routeId}`)
+    .then(response => response.data)
+    .catch(error => error)
+}
+
+export const getStops = async (routeId: string, directionId: number): Promise<Stop[]> => {
+    return await axios.get<Stop[]>(`${baseUrl}/stops/${routeId}/${directionId}`)
+    .then(response => response.data)
+    .catch(error => error)
+}
+
+export const getDepartures = async (routeId: string, directionId: number, stopId: number): Promise<Departure[]> => {
+    return await axios.get<Departure[]>(`${baseUrl}/stops/${routeId}/${directionId}/${stopId}`)
+    .then(response => response.data)
+    .catch(error => error)
+}
