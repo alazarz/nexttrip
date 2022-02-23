b7460f76097813c1e0534c2cb582a5c745f427bd
Alazar Zewde <ayzewde@gmail.com>
2022-02-23 15:43:32 -0600

    remove more dead code

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,12 @@
 import React, { FC } from 'react'
-import { Container, Typography } from '@material-ui/core'
+import { Container } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 interface LayoutProps {
     children: JSX.Element,
 }
 
-const useStyles = makeStyles(() => ({
-    title: { padding: 8}
-}))
-
 export const Layout: FC<LayoutProps> = ({children}) => {
-    const styles = useStyles()
     return (
         <Container maxWidth='sm'>
             {children}
